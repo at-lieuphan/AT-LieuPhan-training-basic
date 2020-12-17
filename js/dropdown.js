dcc8d9848eccfaaf63ff044baf9c01e391a27360
at-lieuphan <lieu.phan@asiantech.vn>
2020-12-17 10:04:40 +0700

    Edit style nav

--- js/dropdown.js
+++ js/dropdown.js
@@ -8,3 +8,12 @@ document.addEventListener('click', function(e) {
     collapse.classList.remove('show');
   }
 })
+
+function clickOpenMenu() {
+  var x = document.getElementById('js-nav-bar');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
