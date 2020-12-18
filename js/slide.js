67a955e4a1bde5ff0651b9e7e70fd8266d769bf7
at-lieuphan <lieu.phan@asiantech.vn>
2020-12-18 09:40:34 +0700

    Add style slide show and carosel

--- js/slide.js
+++ js/slide.js
@@ -1,17 +1,36 @@
-var slideIndex = 1;
-showDivs();
+var slideIndex = 0;
+showSlides();
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName('slide-item');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = 'block';
+  setTimeout(showSlides, 5000);
 }
 
-function showDivs(n) {
+
+//
+var caroselIndex = 0;
+showCarosel();
+
+function showCarosel() {
   var i;
-  var x = document.getElementsByClassName('slide-item');
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = 'none';
+  var slides = document.getElementsByClassName('feedbacks');
+  var dots = document.getElementsByClassName('btn-dot');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';  
   }
-  x[slideIndex-1].style.display = "block";  
+  caroselIndex++;
+  if (caroselIndex > slides.length) {caroselIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' bg-primary', '');
+  }
+  slides[caroselIndex-1].style.display = 'flex';  
+  dots[caroselIndex-1].className += ' bg-primary';
+  setTimeout(showCarosel, 2000);
 }
